3c37555c7ce9d2bd01f44700a2eb0f12bffee443
umitbasaak <030122073@std.izu.edu.tr>
2024-05-28 11:01:45 +0300

    Update proje.py

--- proje.py
+++ proje.py
@@ -160,6 +160,7 @@ if not user_input.isnull().values.any():  # Check if there are no null values
     """, unsafe_allow_html=True)
     
     # Tahmin sonucunu sınıf adıyla birlikte gösterin
+    st.write(knn_user_prediction[0])
     st.write(f"Tahmin: {class_namesEq[knn_user_prediction[0]]}")
 else:
     st.error("Kullanıcı girişi eksik veya hatalı. Lütfen tüm verileri doldurun.")
